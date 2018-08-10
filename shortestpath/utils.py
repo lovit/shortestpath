5434083800df9fa018f4a5fbc34e85baf68d4f3a
lovit <soy.lovit@gmail.com>
2018-08-10 20:38:34 +0900

    Add dict graph transformer

--- shortestpath/utils.py
+++ shortestpath/utils.py
@@ -6,3 +6,14 @@ def load_sparse_graph_and_index(header):
     with open('{}.vocab'.format(header), encoding='utf-8') as f:
         idx2vocab = [vocab.strip() for vocab in f]
     return x, idx2vocab
+
+def to_dict_graph(x, idx2vocab):
+    def to_csr(i):
+        return idx2vocab[i]
+
+    rows, cols = x.nonzero()
+    data = x.data
+    d = defaultdict(lambda: {})
+    for i, j, w in zip(rows, cols, data):
+        d[to_csr(i)][to_csr(j)] = w
+    return dict(d)
