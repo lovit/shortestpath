034240d8afb8a0d4f28a1b5a31914b9ab1bfd2d2
lovit <soy.lovit@gmail.com>
2018-08-13 20:01:15 +0900

    Add usage of Dictionary

--- hmm_pos_tagger/utils.py
+++ hmm_pos_tagger/utils.py
@@ -1,4 +1,17 @@
 class Dictionary:
+"""Usage
+---------
+pos2words = {
+    'Noun': set('아이 아이오 아이오아이 청하 출신'.split()),
+    'Josa': set('은 는 이 가 의 를 을'.split()),
+    'Verb': set('청하 이 있 하 했 입'.split()),
+    'Eomi': set('다 었다 는 니다'.split())
+}
+
+dictionary = Dictionary(pos2words)
+dictionary.get_pos('아이오아이')
+# ['Noun']
+"""
 
     def __init__(self, pos2words=None):
         self.pos2words = pos2words if pos2words else {}
