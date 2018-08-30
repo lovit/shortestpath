50ffe7abd1a1f1c1d3b80c3f8860037c77e46457
lovit <soy.lovit@gmail.com>
2018-08-30 23:57:14 +0900

    Implemented sentence, word lookup

--- cohesion_tokenizer/graph.py
+++ cohesion_tokenizer/graph.py
@@ -6,3 +6,24 @@ class WordSequenceGraph:
 
     def as_graph(self, sentence):
         raise NotImplemented
+
+    def _sentence_lookup(sentence):
+        sent = []
+        for eojeol in sentence.split():
+            sent += word_lookup(eojeol, offset=len(sent))
+        return sent
+
+    def _word_lookup(eojeol, offset=0):
+        n = len(eojeol)
+        # (word, score, begin, end)
+        words = [[(eojeol[i], 0, i, i+1)] for i in range(n)]
+        for b in range(n):
+            for r in range(2, self.cohesion.max_l_length+1):
+                e = b+r
+                if e > n:
+                    continue
+                sub = eojeol[b:e]
+                score = self.cohesion[sub]
+                if score > 0:
+                    words[b].append((sub, score, b, e))
+        return words
