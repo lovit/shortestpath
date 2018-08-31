ddbf7a0516b43e88e1fe78f35e8fdac5f14c8c6b
lovit <soy.lovit@gmail.com>
2018-08-31 16:59:05 +0900

    Add ford with edge list

--- shortestpath/ford.py
+++ shortestpath/ford.py
@@ -6,6 +6,8 @@ from .utils import _find_shortest_path_dict
 def ford(g, start, end, debug=False):
     if isinstance(g, dict):
         return _ford_dict(g, start, end, debug)
+    if isinstance(g, tuple) and (isinstance(g[0], list) and isinstance(g[1], set)):
+        return _ford_list(*g, start, end)
     raise NotImplemented
 
 def _ford_dict(g, start, end, debug=False):
@@ -21,6 +23,49 @@ def _ford_dict(g, start, end, debug=False):
             break
     paths = _find_shortest_path_dict(g, start, end, cost, n_nodes)
     return {'paths': paths, 'cost': cost[end]}
+
+def _ford_list(E, V, S, T):
+
+    ## Initialize ##
+    # (max weight + 1) * num of nodes
+    inf = (max((weight for from_, to_, weight in E)) + 1) * len(V)
+
+    # distance
+    d = {node:0 if node == S else inf for node in V}
+    # previous node
+    prev = {node:None for node in V}
+
+    ## Iteration ##
+    # preventing infinite loop
+    for _ in range(len(V)):
+        # for early stop
+        changed = False
+        for U, V, Wuv in E:
+            d_new = d[U] + Wuv
+            if d_new < d[V]:
+                d[V] = d_new
+                prev[V] = U
+                changed = True
+        if not changed:
+            break
+
+    # Checking negative cycle loop
+    for U, V, Wuv in E:
+        if d[U] + Wuv < d[V]:
+            raise ValueError('Negative cycle exists')
+
+    # Finding path
+    prev_ = prev[T]
+    if prev_ == S:
+        return {'paths':[[prev_, S][::-1]], 'cost':d[T]}
+
+    path = []
+    while prev_ != S:
+        path.append(prev_)
+        prev_ = prev[prev_]
+    path.append(S)
+
+    return {'paths':[path[::-1]], 'cost':d[T]}
 
 def _update_ford_dict(g, cost, debug=False):
     changed = False
